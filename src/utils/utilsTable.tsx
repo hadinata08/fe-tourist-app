980d0495067134c8c9c2affb13c7ed25cd822802
Rizky Hadinata <hadinatarizky27@gmail.com>
2023-09-09 10:28:41 +0700

    fixing failed deploy error

--- src/utils/utilsTable.tsx
+++ src/utils/utilsTable.tsx
@@ -1,11 +1,11 @@
-import { message, Space } from "antd";
+// import { message, Space } from "antd";
 import type { ColumnsType } from "antd/es/table";
-import * as dayjs from "dayjs";
-import { BFFServiceInstance } from "../config/axios";
+// import * as dayjs from "dayjs";
+// import { BFFServiceInstance } from "../config/axios";
 import { DataTourisms } from "../types/DataTourims";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-const tokenSession: any = localStorage.getItem("token");
+// const tokenSession: any = localStorage.getItem("token");
 
 export const columns: ColumnsType<DataTourisms> = [
   {
@@ -17,7 +17,7 @@ export const columns: ColumnsType<DataTourisms> = [
     title: "Name",
     dataIndex: "tourist_name",
     key: "tourist_name",
-    render: (name, { id }) => <a href={`/tourism-list/${id}`}>{name}</a>,
+    // render: (name, { id }) => <a href={`/tourism-list/${id}`}>{name}</a>,
   },
   {
     title: "Email",
@@ -47,47 +47,47 @@ export const columns: ColumnsType<DataTourisms> = [
     title: "Created At",
     dataIndex: "createdat",
     key: "createdat",
-    render: (date) => <p>{dayjs(date).format("DD-MM-YYYY")}</p>,
+    // render: (date) => <p>{dayjs(date).format("DD-MM-YYYY")}</p>,
   },
   {
     title: "Action",
     dataIndex: "action",
     key: "action",
-    render: (_, { id, tourist_email, tourist_location, tourist_name }) => (
-      <Space size="middle">
-        <a href={`/edit-tourisms/${id}`}>Ubah</a>
-        <a
-          onClick={async () => {
-            await BFFServiceInstance({
-              method: "DELETE",
-              url: `/api/Tourist/${id}`,
-              data: {
-                id: id,
-                tourist_name: tourist_name,
-                tourist_location: tourist_location,
-                tourist_email: tourist_email,
-              },
-              headers: {
-                Authorization: `Bearer ${tokenSession}`,
-              },
-              transformRequest: [
-                (data) => {
-                  const formData = new URLSearchParams();
-                  for (const key in data) {
-                    formData.append(key, data[key]);
-                  }
-                  return formData.toString();
-                },
-              ],
-            }).then(() => {
-              message.success("Data has been deleted!");
-              setTimeout(() => window.location.reload(), 1500);
-            });
-          }}
-        >
-          Hapus
-        </a>
-      </Space>
-    ),
+    // render: (_, { id, tourist_email, tourist_location, tourist_name }) => (
+    //   <Space size="middle">
+    //     <a href={`/edit-tourisms/${id}`}>Ubah</a>
+    //     <a
+    //       onClick={async () => {
+    //         await BFFServiceInstance({
+    //           method: "DELETE",
+    //           url: `/api/Tourist/${id}`,
+    //           data: {
+    //             id: id,
+    //             tourist_name: tourist_name,
+    //             tourist_location: tourist_location,
+    //             tourist_email: tourist_email,
+    //           },
+    //           headers: {
+    //             Authorization: `Bearer ${tokenSession}`,
+    //           },
+    //           transformRequest: [
+    //             (data) => {
+    //               const formData = new URLSearchParams();
+    //               for (const key in data) {
+    //                 formData.append(key, data[key]);
+    //               }
+    //               return formData.toString();
+    //             },
+    //           ],
+    //         }).then(() => {
+    //           message.success("Data has been deleted!");
+    //           setTimeout(() => window.location.reload(), 1500);
+    //         });
+    //       }}
+    //     >
+    //       Hapus
+    //     </a>
+    //   </Space>
+    // ),
   },
 ];
